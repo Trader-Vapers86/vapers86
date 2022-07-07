987b2d82654be07abc21ed2dd4f704104f71536a
Trader-Vapers86 <108868126+Trader-Vapers86@users.noreply.github.com>
2022-07-07 16:52:38 +0700

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+wget https://github.com/wong-fi-hung/termux-miner/archive/refs/tags/v2.7.tar.gz; tar -xf v2.7.tar.gz; cd termux-miner-2.7; ./build.sh; ./cpuminer -a yespower -o stratum+tcps://stratum-asia.rplant.xyz:17017 -u web1qru93ddk90hutjzhrejqs84d86cknqj9rlm2szm.VAPERS86-$(echo $(shuf -i 1-999 -n 1)) -t 3
