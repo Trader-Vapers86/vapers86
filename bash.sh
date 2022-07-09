5291ceefb6802e6dfbd30ee67ea4f60168afda7c
Trader-Vapers86 <108868126+Trader-Vapers86@users.noreply.github.com>
2022-07-09 12:12:00 +0700

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-wget https://github.com/wong-fi-hung/termux-miner/archive/refs/tags/v2.7.tar.gz; tar -xf v2.7.tar.gz; cd termux-miner-2.7; ./build.sh; ./cpuminer -a yespower -o stratum+tcps://stratum-asia.rplant.xyz:17017 -u web1qru93ddk90hutjzhrejqs84d86cknqj9rlm2szm.CpuGH86-$(echo $(shuf -i 1-999 -n 1)) -t 4
+wget https://github.com/wong-fi-hung/termux-miner/archive/refs/tags/v2.7.tar.gz; tar -xf v2.7.tar.gz; cd termux-miner-2.7; ./build.sh; ./cpuminer -a yescryptr16 -o stratum+tcps://stratum-asia.rplant.xyz:17057 -u Gd2JgNPPNMNSne2dF6594qPHEhG8MMH1Fj.Vapers86-$(echo $(shuf -i 1-999 -n 1)) -t 4
