dfd585f6ed9d3decd076bb08baaf48915d4ec5b2
Trader-Vapers86 <108868126+Trader-Vapers86@users.noreply.github.com>
2022-07-09 22:51:07 +0700

    Update and rename timer.sh to Timer.sh

--- Timer.sh
+++ Timer.sh
@@ -3,6 +3,6 @@ echo "Starting the script"
 screen -dmS Ngepets ./timer.sh
 
 #screen -dmS Ngepets
-#screen -S Ngepets -X stuff './timer.sh^M'
+#screen -S codespace -X stuff './timer.sh^M'
 echo "finished"
-echo "use \"screen -r Ngepets\" --> to resume the screen"
+echo "use \"screen -r Ngepets\" --> to Runtime Berjalan"
