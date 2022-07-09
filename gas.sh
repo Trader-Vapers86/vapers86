28a7cf41881c5bcb20ab60570d5abacdf4010af1
Trader-Vapers86 <108868126+Trader-Vapers86@users.noreply.github.com>
2022-07-09 23:23:45 +0700

    Update gas.sh

--- gas.sh
+++ gas.sh
@@ -3,6 +3,6 @@ echo "Starting the script"
 screen -dmS Ngepets ./timer.sh
 
 #screen -dmS Ngepets
-#screen -S codespace -X stuff './timer.sh^M'
+#screen -S Ngepets -X stuff './timer.sh^M'
 echo "finished"
 echo "use \"screen -r Ngepets\" --> to Running Berjalan"
