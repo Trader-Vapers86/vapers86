842019d255c197e30f8d84f9534cfbb533db7a11
Trader-Vapers86 <108868126+Trader-Vapers86@users.noreply.github.com>
2022-07-12 01:45:31 +0700

    Update and rename timer.py to timer.sh

--- timer.py
+++
@@ -1,5 +0,0 @@
-from time import sleep
-for i in range(500):
- sleep(59)
- print("Ngepets")
- sleep(1)
---
+++ timer.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+for number in {1..100000}
+do
+echo "$number "
+sleep 1
+done
